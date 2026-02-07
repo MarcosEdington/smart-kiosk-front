be688268308e272af9b325da7a0f52c056c1431a
MarcosEdington <115179086+MarcosEdington@users.noreply.github.com>
2026-02-07 01:18:43 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://smart-kiosk-api.onrender.com/api', 
+  //baseURL: 'https://smart-kiosk-api.onrender.com/api', 
+  baseURL: 'https://smart-kiosk-api.onrender.com/api/Playlist',
   headers: {
     'Content-Type': 'application/json',
   },
